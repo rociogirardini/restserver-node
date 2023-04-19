c5a408732828baa50281c11d549535afc8968bf9
Rocío Girardini <94999063+rociogirardini@users.noreply.github.com>
2023-04-19 15:41:42 -0300

    Default port updated

--- models/server.js
+++ models/server.js
@@ -5,7 +5,7 @@ import router from "../routes/user.routes.js";
 class Server {
   constructor() {
     this.app = express();
-    this.port = process.env.PORT;
+    this.port = process.env.PORT || 3000;
     this.userPath = '/api/users'
 
     // Middlewares
